d0632cfd4dba784d4d53900b4b7f48ff4a5027bc
BhawnaGarg-2508 <bhawna.garg20@st.niituniversity.in>
2023-06-29 17:07:04 +0530

    added flask basic routes

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<h1>Hello, World!</h1>"
+
+@app.route("/about")
+def hello_world2():
+    return "<h1>This is about</h1>"
+
+@app.route("/test2")
+def test2():
+    data = request.args.get('x')
+    return "This is about input data from url {}".format(data)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
